c36915da7215aa6c1e4361b2ab3e4d10199b473d
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-07-29 21:36:24 -0400

    pre-deploy

--- controllers/register.js
+++ controllers/register.js
@@ -1,34 +1,46 @@
-const handleRegister =  (db, bcrypt) => (req, res) => {
+const handleRegister = (db, bcrypt) => (req, res) => {
     const { email, name, password } = req.body;
+  
     if (!email || !name || !password) {
-        return res.status(400).json('Incorrect form submission')
+      return res.status(400).json('Incorrect form submission');
     }
+  
     const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
+  
+    db.transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
         })
         .into('login')
         .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
+        .then((loginEmail) => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              email: loginEmail[0],
+              name: name,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user[0]);
+            })
+            .catch((error) => {
+              res.status(400).json('Unable to insert user into users table');
+              console.error('Error inserting user into users table:', error);
+            });
         })
         .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-}
-
-module.exports = {
-    handleRegister: handleRegister
-}
+        .catch((error) => {
+          trx.rollback();
+          res.status(400).json('Unable to register');
+          console.error('Error registering user:', error);
+        });
+    });
+  };
+  
+  module.exports = {
+    handleRegister: handleRegister,
+  };
+  
