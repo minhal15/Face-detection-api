09d13fae9111d3477bf9c65922c7efec28f65e04
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-07-30 01:35:26 -0400

    pre-deploy

--- controllers/register.js
+++ controllers/register.js
@@ -4,40 +4,35 @@ const handleRegister = (db, bcrypt) => (req, res) => {
         return res.status(400).json('Incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    trx.commit(); // Add this line to commit the transaction
-                    console.log('Transaction committed successfully.');
-                    res.json(user[0]);
-                })
-                .catch(err => {
-                    trx.rollback(); // Add this line to rollback the transaction in case of an error
-                    console.error('Error inserting into users table:', err);
-                    res.status(500).json('unable to register to user table');
-                });
-        })
-        .catch(err => {
-            console.error('Error inserting into login table:', err);
-            res.status(500).json('unable to register to login table');
-        });
+
+    // Try a simple insert without transactions
+    db.insert({
+        hash: hash,
+        email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+        // Insert into 'users' table (without transactions for testing)
+        db('users')
+            .returning('*')
+            .insert({
+                email: loginEmail[0].email,
+                name: name,
+                joined: new Date()
+            })
+            .then(user => {
+                console.log('User inserted successfully:', user);
+                res.json(user[0]);
+            })
+            .catch(err => {
+                console.error('Error inserting into users table:', err);
+                res.status(500).json('unable to register to users table');
+            });
     })
     .catch(err => {
-        console.error('Error starting transaction:', err);
-        res.status(500).json('unable to start transaction');
+        console.error('Error inserting into login table:', err);
+        res.status(500).json('unable to register to login table');
     });
 };
 
