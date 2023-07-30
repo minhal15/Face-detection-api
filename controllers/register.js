a46c005195593a954903536386f94af55c99fd9a
minhal15 <64689808+minhal15@users.noreply.github.com>
2023-07-30 01:23:31 -0400

    pre-deploy

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,7 @@
-const handleRegister =  (db, bcrypt) => (req, res) => {
+const handleRegister = (db, bcrypt) => (req, res) => {
     const { email, name, password } = req.body;
     if (!email || !name || !password) {
-        return res.status(400).json('Incorrect form submission')
+        return res.status(400).json('Incorrect form submission');
     }
     const hash = bcrypt.hashSync(password);
     db.transaction(trx => {
@@ -20,15 +20,27 @@ const handleRegister =  (db, bcrypt) => (req, res) => {
                     joined: new Date()
                 })
                 .then(user => {
+                    trx.commit(); // Add this line to commit the transaction
+                    console.log('Transaction committed successfully.');
                     res.json(user[0]);
                 })
+                .catch(err => {
+                    trx.rollback(); // Add this line to rollback the transaction in case of an error
+                    console.error('Error inserting into users table:', err);
+                    res.status(500).json('unable to register to user table');
+                });
         })
-        .then(trx.commit)
-        .catch(trx.rollback)
+        .catch(err => {
+            console.error('Error inserting into login table:', err);
+            res.status(500).json('unable to register to login table');
+        });
     })
-    .catch(err => res.status(400).json('unable to register to user table'))
-}
+    .catch(err => {
+        console.error('Error starting transaction:', err);
+        res.status(500).json('unable to start transaction');
+    });
+};
 
 module.exports = {
     handleRegister: handleRegister
-}
+};
